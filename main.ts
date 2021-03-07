4c7b371df9a8349de976bac649e8693e45d91621
Raymon S <raymon@raymonschouwenaar.nl>
2021-03-07 20:09:33 +0100

    Added tweet styling

--- main.ts
+++ main.ts
@@ -6,7 +6,9 @@ const outputTxt : HTMLDivElement = document.querySelector('#output')
 const generate : HTMLDivElement = document.querySelector('#generate')
 const date : HTMLSpanElement = document.querySelector('#date')
 
-date.innerText = new Intl.DateTimeFormat('en-US', {day: 'numeric', month: 'long', year:  'numeric'}).format(new Date())
+const today = new Intl.DateTimeFormat('en-US', {day: 'numeric', month: 'long', year:  'numeric'}).format(new Date())
+
+date.innerText = today
 
 const text = `Webtwo ipsum bitly convore scribd hipmunk, elgg. Ebay empressr blekko prezi, etsy quora. Plugg groupon bitly wakoopa tumblr zanga, wesabe doostang blekko. lijit. Heekya orkut odeo zillow reddit rovio, imeem rovio koofers plugg, empressr squidoo kippt omgpop. Jaiku napster hulu waze skype orkut, bubbli yuntaa babblely plugg blyve, greplin twones plickers gooru. Chumby jajah kazaa yoono odeo koofers woopra balihoo, eskobo blekko napster imvu sococo octopart. Loopt zooomr joukuu revver dropio mog, lanyrd heroku orkut. Jumo revver klout zillow, insala.
 
@@ -25,7 +27,6 @@ generate.addEventListener('click', function() {
     console.log('item: ', item)
     return item.length > 0 
   })
-  console.log("🚀 ~ file: main.ts ~ line 21 ~ filtered ~ filtered", filtered)
 
   const split = filtered.map((string) => tweetSplitter(string, 276, ''))
 
@@ -33,18 +34,48 @@ generate.addEventListener('click', function() {
 
   console.log('flat: ', flat)
 
-  flat.forEach((string, index) => {
-    let newStr = ''
-    console.log('tweets: ', string)
-    newStr += `${index + 1}${'\\'}${flat.length} ${string} \n\n`
-    tweets += newStr
-  })
-
-  const txt = document.createTextNode(tweets)
-  
-  outputTxt.appendChild(txt)
-  
+  flat.forEach((string, index) => createTweetElement(flat, string, index))
 })
+
+const username = '@devbyrayray'
+
+function createTweetElement(arr, string, index) {
+  const tweetEl = document.createElement('div')
+  tweetEl.className = 'tweet'
+
+  const tweetHeader = document.createElement('div')
+  tweetHeader.className = 'tweet__header'
+
+  const tweetUserEl = document.createElement('strong')
+  const tweetDateEl = document.createElement('em')
+
+  const tweetUser = document.createTextNode(username)
+  const tweetDate = document.createTextNode(today)
+  tweetUserEl.appendChild(tweetUser)
+  tweetDateEl.appendChild(tweetDate)
+
+  tweetHeader.appendChild(tweetUserEl)
+  tweetHeader.appendChild(tweetDateEl)
+
+  const tweetContentEl = document.createElement('div')
+  tweetContentEl.className = 'tweet__content'
+
+  const tweetProfile = document.createElement('img')
+  tweetProfile.className = 'tweet__profile'
+  tweetProfile.src = 'https://pbs.twimg.com/profile_images/1366726728345915392/hzK0Vj4Z_400x400.jpg'
+  tweetProfile.loading = 'lazy'
+  
+  const newStr = `${index + 1}${'\\'}${arr.length} ${string}`
+  const tweetTxt = document.createTextNode(newStr)
+
+
+
+  tweetContentEl.appendChild(tweetHeader)
+  tweetContentEl.appendChild(tweetTxt)
+  tweetEl.appendChild(tweetProfile)
+  tweetEl.appendChild(tweetContentEl)
+  outputTxt.appendChild(tweetEl)
+}
 
 inputTxt.value = text
 
